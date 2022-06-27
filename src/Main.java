a6a88740219878991491e67d6799a92d0314eac5
shaik-md-ibnay-momen <shaikmohammad864@gmail.com>
2022-06-27 12:51:32 +0600

    Selenium setup

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
+    static void driver() {
+        System.setProperty("webdriver.chrome.driver", "F:\\Selenium\\Selenium Testing\\Selenium Jar & Drivers\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com/");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        driver();
     }
 }
