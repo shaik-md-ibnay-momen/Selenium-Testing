ef4d29a8906a661345f5d1c9e536b6855c584fdf
shaik-md-ibnay-momen <shaikmohammad864@gmail.com>
2022-06-27 16:44:05 +0600

    Banking Login ok but option not working

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class Main {
     private static WebDriver driver;
@@ -11,11 +13,25 @@ public class Main {
 
     static void browser_link(){
         driver.manage().window().maximize();
-        driver.get("https://www.google.com/");
+        driver.get("https://demo.guru99.com/v4/index.php");
+    }
+
+    static void login(){
+        driver.findElement(By.xpath("/html/body/form/table/tbody/tr[1]/td[2]/input")).sendKeys("mngr419643");
+        driver.findElement(By.xpath("/html/body/form/table/tbody/tr[2]/td[2]/input")).sendKeys("ebEZAqy");
+        driver.findElement(By.xpath("/html/body/form/table/tbody/tr[3]/td[2]/input[1]")).click();
+    }
+
+    static void menu(){
+        driver.findElement(By.xpath("/html/body/div[3]/div/ul/li[2]")).click();
     }
 
     public static void main(String[] args) {
         driver();
         browser_link();
+        login();
+        menu();
+
+
     }
 }
