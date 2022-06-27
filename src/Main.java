b53b497bd9737b6689eae818614bedf95afe734d
shaik-md-ibnay-momen <shaikmohammad864@gmail.com>
2022-06-27 14:39:29 +0600

    functions

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
-    static void driver() {
+    private static WebDriver driver;
+
+    public static void driver() {
         System.setProperty("webdriver.chrome.driver", "F:\\Selenium\\Selenium Testing\\Selenium Jar & Drivers\\chromedriver_win32\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
+        link();
+    }
+
+    static void link(){
         driver.manage().window().maximize();
         driver.get("https://www.google.com/");
     }
 
     public static void main(String[] args) {
         driver();
+
     }
 }
